a7b2243cff19c9afcd0614441a0920fa49d5d99a
ericknathanael <70834101+ericknathanael@users.noreply.github.com>
2021-05-03 15:51:12 +0700

    Add files via upload

---
+++ ProjectPCS/ProjectPCS/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Oracle.DataAccess.Client;
+
+namespace ProjectPCS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static OracleConnection conn;
+        public static string source, userId, pass;
+        public MainWindow()
+        {
+            InitializeComponent();
+            try
+            {
+                source = tbData.Text;
+                userId = tbUser.Text;
+                pass = tbPass.Text;
+                conn = new OracleConnection("Data Source = " + source + "; User ID = " + userId + "; password = " + pass);
+                loginWindow menu = new loginWindow();
+                this.Hide();
+                menu.ShowDialog();
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btSubmit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ProjectPCS/ProjectPCS/loginWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Oracle.DataAccess.Client;
+
+namespace ProjectPCS
+{
+    /// <summary>
+    /// Interaction logic for loginWindow.xaml
+    /// </summary>
+    public partial class loginWindow : Window
+    {
+        OracleConnection conn; 
+        public loginWindow()
+        {
+            InitializeComponent();
+            conn = MainWindow.conn;
+        }
+
+        private void btRegis_Click(object sender, RoutedEventArgs e)
+        {
+
+            registerWindow menu = new registerWindow();
+            this.Close();
+            menu.ShowDialog();
+            this.Hide();
+        }
+    }
+}
